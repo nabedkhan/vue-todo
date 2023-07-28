448c088f7f19fda06d89dadbd305943078386e1c
Nabed Khan <nabed420@gmail.com>
2023-07-29 00:02:09 +0600

    validation added

--- src/components/for-app-3/TodoCreateForm.vue
+++ src/components/for-app-3/TodoCreateForm.vue
@@ -1,24 +1,47 @@
 <template>
-  <form class="flex items-center w-full" @submit.prevent="$emit('create')">
+  <form class="flex items-center w-full" @submit.prevent="handleSubmit">
     <input
       type="text"
       :value="modelValue"
       @input="$emit('update:modelValue', $event.target.value)"
       class="border border-gray-700 bg-transparent p-3 px-5 rounded-l-lg outline-none w-full"
+      :class="{ 'border-red-500': todoInputFieldError }"
     />
 
     <input
       type="submit"
       value="Add"
-      class="bg-gray-100 text-gray-800 p-3 px-6 rounded-r-lg tex-lg font-medium border border-gray-100"
+      class="bg-gray-100 text-gray-800 p-3 px-6 rounded-r-lg tex-lg font-medium border border-gray-100 cursor-pointer"
+      :class="{ 'border-red-500 bg-red-500': todoInputFieldError }"
     />
   </form>
+
+  <span v-if="todoInputFieldError" class="text-red-500 mt-2 block">
+    {{ todoInputFieldError }}
+  </span>
 </template>
 
 <script>
 export default {
   props: {
     modelValue: String,
+  },
+
+  data() {
+    return {
+      todoInputFieldError: "",
+    };
+  },
+
+  methods: {
+    handleSubmit() {
+      if (this.modelValue) {
+        this.$emit("create");
+        return;
+      }
+
+      this.todoInputFieldError = "Todo Title is required!";
+    },
   },
 };
 </script>
